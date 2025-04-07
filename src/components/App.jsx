4cd528604f709c39a97a66de865533e2e68118eb
Bohdan Vasylovych <war.mant1kor@gmail.com>
2025-04-07 13:43:31 +0300

    GoIT React Home Work №3

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import Container from "./Container/Container.jsx";
+import "../components/App.scss";
+
+const App = () => {
+  return (
+    <Container>
+      <div className="signature">
+        <a href="https://github.com/Mrmantikor" target="_blank">
+          Bohdan_Vasylovych
+        </a>
+      </div>
+    </Container>
+  );
+};
+
+export default App;
---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,8 @@
+import s from './Container.module.css';
+import clsx from 'clsx';
+
+const Container = ({ children }) => {
+  return <div className={clsx(s.container)}>{children}</div>;
+};
+
+export default Container;
